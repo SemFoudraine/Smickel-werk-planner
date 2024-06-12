f908f780cd20e44c23c6f5739ad911a8d0a6d7f1
Sem Foudraine <129499727+SemFoudraine@users.noreply.github.com>
2024-06-12 13:00:59 +0200

    api beveiligd

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,15 @@ use App\Http\Controllers\TaskController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::get('/roosterdata', [RoosterController::class, 'getRoosterData']);
 Route::post('login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->get('/userdata', [UserController::class, 'getUser']);
-Route::middleware('auth:api')->get('/user/hours', [DashboardController::class, 'getUserHours']);
-Route::middleware('auth:api')->get('/tasks', [TaskController::class, 'getTasks']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/roosterdata', [RoosterController::class, 'getRoosterData']);
+    Route::get('/userdata', [UserController::class, 'getUser']);
+    Route::get('/user/hours', [DashboardController::class, 'getUserHours']);
+    Route::get('/tasks', [TaskController::class, 'getTasks']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
